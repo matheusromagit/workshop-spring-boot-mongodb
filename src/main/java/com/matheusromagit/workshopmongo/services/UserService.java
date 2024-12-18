f5106734a03303214f0cd62777d62bd3fb78fb45
Matheus Roma <matheusroma.ctt@gmail.com>
2024-12-18 10:53:29 -0300

    Atualização de usuário com PUT

--- src/main/java/com/matheusromagit/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusromagit/workshopmongo/services/UserService.java
@@ -36,7 +36,19 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getEmail());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto	) {
 		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
+	
 }
